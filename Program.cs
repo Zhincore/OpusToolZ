8cc0085ec084ee230f6c904448f079617a796058
Zhincore <adam@zhincore.eu>
2023-10-20 21:20:32 +0200

    Trim the main program to be non-interactive and single-purpose

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using WolvenKit.Modkit.RED4.Opus;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace OpusToolZ
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string opusinfo = args[0];
+            string filehash = args[1];
+
+            FileStream fs = new FileStream(opusinfo, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            fs.Position = 0;
+            if (BitConverter.ToString(br.ReadBytes(3)) != "53-4E-44")
+            {
+                fs.Dispose();
+                fs.Close();
+                Console.WriteLine("Not a opusinfo file");
+                return 1;
+            }
+
+            var info = new OpusInfo(fs);
+
+            List<UInt16> tempolisto = new List<UInt16>();
+            for (int i = 0; i < info.OpusCount; i++)
+            {
+                if(!tempolisto.Contains(info.PackIndices[i]))
+                {
+                    tempolisto.Add(info.PackIndices[i]);
+                }
+            }
+
+            UInt32 numOfPaks = Convert.ToUInt32(tempolisto.Count);
+
+            string[] files = Directory.GetFiles(Path.GetDirectoryName(opusinfo), "*.opuspak").OrderBy(_ => Convert.ToUInt32(_.Replace(".opuspak", string.Empty).Substring(_.LastIndexOf('_') + 1))).ToArray();
+            Stream[] streams = new Stream[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                streams[i] = new FileStream(files[i], FileMode.Open, FileAccess.Read);
+            }
+            
+            if(files.Length != numOfPaks)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("All " + Convert.ToString(numOfPaks) + " .opuspak files are not present in the directory of sfx_container.opusinfo");
+                return 1;
+            }
+
+
+            UInt32 hash = 0;
+            try
+            {
+                hash = Convert.ToUInt32(filehash);
+                if (!info.OpusHashes.Contains(hash))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Opus file hash not present in opusinfo , try again");
+                    Console.ResetColor();
+                }
+            }
+            catch { 
+                Console.WriteLine("Invalid Hash"); 
+                return 1; 
+            }
+
+
+            string output = AppDomain.CurrentDomain.BaseDirectory + filehash + ".opus";
+            info.WriteOpusFromPaks(streams, new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory), hash);
+            
+
+            Console.WriteLine("output: " + output);
+            return 0;            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,22 +1,3 @@
-# Cyberpunk 2077 Research
+# Opuspak CLI
 
-This repository was created to help study and reverse engineer elements of Cyberpunk 2077 running on REDengine 4. The focus is researching RED4 file formats and engineering solutions for visualizing and modifying them.
-
----
-
-# Cyberpunk Blender Add-on
-
-![image](https://user-images.githubusercontent.com/65016231/130711162-301926fa-c7a0-4e08-b7c7-414bfa558205.png)
-
-
-The Cyberpunk Blender add-on is designed to fully automate the shader setup for Cyberpunk 2077 mesh files. The add-on integrates directly with WolvenKit by making use of a bespoke material json file. To learn more about creating material json files please visit the [dedicated WolvenKit wiki page.](https://wiki.redmodding.org/wolvenkit/wolvenkit-app/editor/import-and-export-tool/blender-integration)
-
-The Cyberpunk add-on requires Blender 2.92 or higher. Not all Cyberpunk shaders are currently supported by the add-on. If you're interested in adding support for a new shader please consider reaching out to us on our Discord, [Cyberpunk 2077 Modding Community.](https://discord.gg/Epkq79kd96)
-
-[Read more about Cyberpunk shaders and materials on the RED Modding wiki](https://wiki.redmodding.org/redengine4-research/assets/shaders)
-
-  [Research Documentation](./docs/README.md)
- </p>
-  <p align="center">
-    <a href="https://www.patreon.com/HitmanHimself"><img src="https://i.ibb.co/RBZKRg4/Become-a-patron-button.png" alt="Become-a-patron-button" border="0"></a>
- </p>
+Taken from https://github.com/HitmanHimself/cp77research and modified for my purposes.
